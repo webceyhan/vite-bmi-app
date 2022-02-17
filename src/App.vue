130d25eb5f383e7028eb4bacc5b02115e46caeaf
Evren Ceyhan <evren@ceyhan.io>
2022-02-17 16:50:58 +0300

    bmi added with progress calculation

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,25 @@ const form = ref({
   height: 0,
 });
 
-const calories = computed(() => {
+const bmiMap = {
+  underweight: {  label: 'Underweight', color: 'info' },
+  normal: { label: 'Normal', color: 'success' },
+  overweight: { label: 'Overweight', color: 'warning' },
+  obesity: { label: 'Obesity', color: 'danger' },
+}
+
+const bmi = computed(() => {
+  const { weight, height } = form.value;
+  const value = ((weight / height / height) * 10000).toFixed(2);
+
+  if (value < 18.5) return { ...bmiMap.underweight, value };
+  if (value < 25) return { ...bmiMap.normal, value };
+  if (value < 30) return { ...bmiMap.overweight, value };
+  return { ...bmiMap.obesity, value };
+
+})
+
+const bmr = computed(() => {
   // get input values
   const { age, weight, height, gender } = form.value;
 
@@ -23,7 +41,7 @@ const calories = computed(() => {
 
 <template>
   <div class="container p-lg-5">
-    <h1 class="display-5">BMI Calculator</h1>
+    <h1 class="display-5">BMI/BMR Calculator</h1>
 
     <div class="card bg-light mb-4">
       <div class="card-body p-4">
@@ -88,11 +106,24 @@ const calories = computed(() => {
 
     <div class="card">
       <div class="card-body p-4">
-        <h6 class="card-title">The Basal Metabolic Rate (BMR):</h6>
+        <h4 class="card-title">Your results</h4>
 
-        <p class="card-text">
-          BMR =
-          <span class="text-success">{{ calories }}</span> kcal / day
+        <hr />
+
+        <p class="mb-2">
+          Body Mass Index (BMI):
+          <span :class="`text-${bmi.color}`">{{ bmi.value }}</span>
+        </p>
+
+        <div class="progress mb-4" style="height: 1.5rem;">
+          <div :class="`progress-bar bg-${bmi.color}`" :style="{width: `${bmi.value}%`}">
+          {{bmi.label}}
+          </div>
+        </div>
+
+        <p class="mb-2">
+          The Basal Metabolic Rate (BMR):
+          <span :class="`text-${bmi.color}`">{{ bmr }}</span> kcal / day
         </p>
 
         <!-- <input class="form-control form-control-lg" readonly v-model="calories" /> -->
